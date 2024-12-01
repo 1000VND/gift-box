efd017d29f8995b0c9c0320fb9a3d8f9df318292
hunglq <104688414+1001VND@users.noreply.github.com>
2024-12-02 00:06:25 +0700

    change router

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,7 +1,16 @@
-import { Routes } from '@angular/router';
+import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
+import { NgModule } from '@angular/core';
+import { HashLocationStrategy, LocationStrategy } from '@angular/common';
 
 export const routes: Routes = [
     { path: '', pathMatch: 'full', redirectTo: '/birthday' },
     { path: 'birthday', component: HomeComponent },
 ];
+
+@NgModule({
+    imports: [RouterModule.forRoot(routes)],
+    providers: [{ provide: LocationStrategy, useClass: HashLocationStrategy }],
+    exports: [RouterModule],
+})
+export class AppRoutingModule { }
